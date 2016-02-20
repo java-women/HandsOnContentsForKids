f4b504d56627af9f0251e7d0dc3742112fe2cd5a
erk5 <e.hirajiri@gmail.com>
2016-02-20 23:35:25 +0900

    Add editor.

--- JavajoTeachingForKids/src/main/resources/static/js/stage001.js
+++ JavajoTeachingForKids/src/main/resources/static/js/stage001.js
@@ -1,161 +1,233 @@
 enchant();
 
-window.onload = function(){
+var core;
+var inputYourLife = 10;
+var inputYourAttack = 1;
+var inputEnemyLife = 10000;
+var inputEnemyAttack = 2;
 
-    var core = new Core(320, 320);
-    core.preload("avatarBg1.png", "avatarBg2.png", "avatarBg3.png", "monster/bigmonster1.gif", "clear.png");
+window.onload = function() {
+
+    core = new Core(320, 320);
     core.fps = 15;
-    core.rootScene.backgroundColor = "gray";
+    core.preload("avatarBg1.png", "avatarBg2.png", "avatarBg3.png", "monster/bigmonster1.gif");
+    core.preload("start.png", "gameover.png", "clear.png");
 
-    core.onload = function(){
-        /* 背景:砂漠 */
-        var bg = new AvatarBG(0);
-        core.rootScene.addChild(bg);
-
-        /*
-         * キー割り当て
-         *
-         * [space]キー：b
-          */
-        core.keybind(' '.charCodeAt(0), 'b');
-
-        /*
-         * アバター
-         *
-         * 以下のアバターエディタで性別・武器・服などを組み合わせて生成されたコードをAvatarのコンストラクタに設定する.
-         * http://9leap.net/games/1383/
-         * Chromeだと動かなかった.
-         */
-        var avatar = new Avatar("2:6:1:2009:21013:22480");
-        avatar.moveTo(30, 100);
-        avatar.action = "run";
-        avatar.life = 10;
-        core.rootScene.addChild(avatar);
-
-        /* エネミー */
-        var enemy = new AvatarMonster(core.assets["monster/bigmonster1.gif"]);
-        enemy.moveTo(210, 80);
-        enemy.action = "walk";
-        var param = getParameter();
-        enemy.life = param["enemy"];
-        core.rootScene.addChild(enemy);
-
-        /* アバター動作 */
-        avatar.addEventListener('enterframe', function() {
-            /* 左右のみ移動可 */
-            if (core.input.left) this.x -= 5;
-            if (core.input.right) this.x += 5;
-
-            if (this.x <= 0) this.x = 0;
-            if (this.x > 260) this.x = 260;
-
-            /** 攻撃 */
-            if (core.input.b) {
-                avatar.action = "attack";
-                if (this.intersect(enemy) && enemy.life > 0) {
-                    enemy.life -= 1;
-                }
-            }
-
-            if (avatar.life <= 0) avatar.action = "dead";
-
-            avatarLifeBar.width = avatar.life * 20;
-
-            if (avatar.life == 0) {
-                core.end();}
-        });
-
-        /* エネミー動作 */
-        enemy.addEventListener('enterframe', function() {
-            /** 攻撃 */
-            if (core.frame % 100 == 0) {
-                enemy.action = "attack";
-                if (this.intersect(avatar) && avatar.life > 0) {
-                    avatar.action = "damage";
-                    avatar.life -= 2;
-                }
-            }
-
-            enemyLifeBar.width = enemy.life * (200 / param["enemy"]);
-
-            if (enemy.life <= 0) {
-                enemy.action = "dead";
-                core.stop();
-                var clear = new Sprite(270, 48);
-                clear.image = core.assets["clear.png"];
-                clear.x = 25;
-                clear.y = 140;
-                core.rootScene.addChild(clear);
-            }
-        });
-
-        /* 操作説明 */
-        var label = new Label();
-        label.x = 10;
-        label.y = 220;
-        label.text = '移動： ←→<br>攻撃：スペース';
-        core.rootScene.addChild(label);
-
-        /* アバターライフゲージ */
-        var avatarLife = new Label();
-        avatarLife.x = 10;
-        avatarLife.y = 180;
-        avatarLife.text = 'Your Life';
-        core.rootScene.addChild(avatarLife);
-
-        var avatarLifeBar = new Entity();
-        avatarLifeBar.width = 200;
-        avatarLifeBar.height = 10;
-        avatarLifeBar.backgroundColor = '#00ff00';
-        avatarLifeBar.x = 80;
-        avatarLifeBar.y = 180;
-        core.rootScene.addChild(avatarLifeBar);
-
-        /* エネミーライフゲージ */
-        var enemyLife = new Label();
-        enemyLife.x = 10;
-        enemyLife.y = 200;
-        enemyLife.text = 'Enemy Life';
-        core.rootScene.addChild(enemyLife);
-
-        var enemyLifeBar = new Entity();
-        enemyLifeBar.width = 200;
-        enemyLifeBar.height = 10;
-        enemyLifeBar.backgroundColor = '#ff0000';
-        enemyLifeBar.x = 80;
-        enemyLifeBar.y = 200;
-        core.rootScene.addChild(enemyLifeBar);
+    core.onload = function() {
+        core.replaceScene(createStartScene());
     };
 
     core.start();
 };
 
-/*
- * URLのパラメータを取得.
+/**
+ * パラメータ編集
  */
-function getParameter() {
-    var key=new Array();
-    key["enemy"] = 10000;
-
-    var param = location.search;
-    if (!param) return key;
-    param = param.substring(1);
-    var pair = param.split("&");
-    var i=temp="";
-    for (i=0; i < pair.length; i++) {
-        temp=pair[i].split("=");
-        keyName=temp[0];
-        keyValue=temp[1];
-        key[keyName]=keyValue;
-    }
-
-    /* エネミーのデフォルトHP設定 */
-     if (!key["enemy"] || key["enemy"] == "" || isNaN(unescape(key["enemy"]))){
-        key["enemy"] = 10000;
-    }else{
-        /* コード変換 */
-        name = key["enemy"];
-    }
-
-    return key;
+function editData(){
+    inputYourLife = document.getElementById("your-life").value;
+    inputYourAttack = document.getElementById("your-attack").value;
+    inputEnemyLife = document.getElementById("enemy-life").value;
+    inputEnemyAttack = document.getElementById("enemy-attack").value;
+    core.replaceScene(createStartScene());
 }
+
+/**
+ * タイトルシーン
+ */
+function createStartScene() {
+    var scene = new Scene();
+    scene.backgroundColor = '#3cb371';
+    var startImage = new Sprite(236, 48);
+    startImage.image = core.assets['start.png'];
+    startImage.x = 42;
+    startImage.y = 136;
+    scene.addChild(startImage);
+
+    var title = new Label('敵を倒そう！');
+    title.textAlign = 'center';
+    title.color = '#ffffff';
+    title.x = 20;
+    title.y = 96;
+    title.font = '28px sans-serif';
+    scene.addChild(title);
+
+    startImage.addEventListener(Event.TOUCH_START, function(e) {
+        core.replaceScene(createGameScene());
+    });
+
+    return scene;
+};
+
+/**
+ * ゲームシーン
+ */
+function createGameScene() {
+    var scene = new Scene();
+
+    /* 背景:砂漠 */
+    var bg = new AvatarBG(0);
+    scene.addChild(bg);
+    scene.backgroundColor = "#303030";
+
+    /*
+     * キー割り当て
+     *
+     * [space]キー：b
+      */
+    core.keybind(' '.charCodeAt(0), 'b');
+
+    /*
+     * アバター
+     *
+     * 以下のアバターエディタで性別・武器・服などを組み合わせて生成されたコードをAvatarのコンストラクタに設定する.
+     * http://9leap.net/games/1383/
+     * Chromeだと動かなかった.
+     */
+    var avatar = new Avatar("2:6:1:2009:21013:22480");
+    avatar.moveTo(30, 100);
+    avatar.action = "run";
+    avatar.life = parseInt(inputYourLife);
+    avatar.attack = parseInt(inputYourAttack);
+    scene.addChild(avatar);
+
+    /* エネミー */
+    var enemy = new AvatarMonster(core.assets["monster/bigmonster1.gif"]);
+    enemy.moveTo(210, 80);
+    enemy.action = "walk";
+    enemy.life = parseInt(inputEnemyLife);
+    enemy.attack = parseInt(inputEnemyAttack);
+    scene.addChild(enemy);
+
+    /* アバター動作 */
+    avatar.addEventListener('enterframe', function() {
+        /* 左右のみ移動可 */
+        if (core.input.left) this.x -= 5;
+        if (core.input.right) this.x += 5;
+
+        if (this.x <= 0) this.x = 0;
+        if (this.x > 260) this.x = 260;
+
+        /** 攻撃 */
+        if (core.input.b) {
+            avatar.action = "attack";
+            if (this.intersect(enemy) && enemy.life > 0) {
+                enemy.life -= avatar.attack;
+            }
+        }
+
+        if (avatar.life <= 0) avatar.action = "dead";
+
+        avatarLifeBar.width = avatar.life * (200 / inputYourLife);
+
+        if (avatar.life == 0) {
+            core.replaceScene(createGameoverScene());
+        }
+    });
+
+    /* エネミー動作 */
+    enemy.addEventListener('enterframe', function() {
+        /** 攻撃 */
+        if (core.frame % 100 == 0) {
+            enemy.action = "attack";
+            if (this.intersect(avatar) && avatar.life > 0) {
+                avatar.action = "damage";
+                avatar.life -= enemy.attack;
+            }
+        }
+
+        enemyLifeBar.width = enemy.life * (200 / inputEnemyLife);
+
+        if (enemy.life <= 0) {
+            enemy.action = "dead";
+            core.replaceScene(createGameclearScene());
+        }
+    });
+
+    /* 操作説明 */
+    var label = new Label('移動： ←→<br>攻撃：スペース');
+    label.color = '#ffffff';
+    label.font = '20px sans-serif';
+    label.x = 10;
+    label.y = 220;
+    scene.addChild(label);
+
+    /* アバターライフゲージ */
+    var avatarLife = new Label('Your Life');
+    avatarLife.color = '#ffffff';
+    avatarLife.font = '20px sans-serif';
+    avatarLife.x = 10;
+    avatarLife.y = 180;
+    scene.addChild(avatarLife);
+
+    var avatarLifeBar = new Entity();
+    avatarLifeBar.width = 200;
+    avatarLifeBar.height = 15;
+    avatarLifeBar.backgroundColor = '#00ff00';
+    avatarLifeBar.x = 110;
+    avatarLifeBar.y = 180;
+    scene.addChild(avatarLifeBar);
+
+    /* エネミーライフゲージ */
+    var enemyLife = new Label('Enemy Life');
+    enemyLife.color = '#ffffff';
+    enemyLife.font = '20px sans-serif';
+    enemyLife.x = 10;
+    enemyLife.y = 200;
+    scene.addChild(enemyLife);
+
+    var enemyLifeBar = new Entity();
+    enemyLifeBar.width = 200;
+    enemyLifeBar.height = 15;
+    enemyLifeBar.backgroundColor = '#ff0000';
+    enemyLifeBar.x = 110;
+    enemyLifeBar.y = 200;
+    scene.addChild(enemyLifeBar);
+
+    return scene;
+};
+
+/**
+ * ゲームクリアシーン
+ */
+function createGameclearScene() {
+    var scene = new Scene();
+    scene.backgroundColor = '#303030';
+
+    var gameclearImage = new Sprite(189, 97);
+    gameclearImage.image = core.assets['clear.png'];
+    gameclearImage.x = 65;
+    gameclearImage.y = 112;
+    scene.addChild(gameclearImage);
+
+    var retryButton = new Button("もう一度遊ぶ", "light");
+    retryButton.moveTo(110,230);
+    scene.addChild(retryButton);
+    retryButton.ontouchstart = function(){
+        core.replaceScene(createStartScene());
+    }
+
+    return scene;
+};
+
+/**
+ * ゲームオーバーシーン
+ */
+function createGameoverScene() {
+    var scene = new Scene();
+    scene.backgroundColor = '#303030';
+
+    var gameoverImage = new Sprite(189, 97);
+    gameoverImage.image = core.assets['gameover.png'];
+    gameoverImage.x = 65;
+    gameoverImage.y = 112;
+    scene.addChild(gameoverImage);
+
+    var retryButton = new Button("もう一度遊ぶ", "light");
+    retryButton.moveTo(110,230);
+    scene.addChild(retryButton);
+    retryButton.ontouchstart = function(){
+        core.replaceScene(createStartScene());
+    }
+
+    return scene;
+};
