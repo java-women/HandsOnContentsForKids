f2ae8d4bb87517703d68fd87b7451b2ce21b4e6d
toon258 <to258_yuri@yahoo.co.jp>
2016-03-08 02:44:15 +0900

    masterよりマージ

--- JavajoTeachingForKids/src/main/resources/static/js/stage002.js
+++ JavajoTeachingForKids/src/main/resources/static/js/stage002.js
@@ -3,14 +3,14 @@ enchant();
 window.onload = function(){
     var game = new Core(320, 320);
     game.fps = 15;
-    game.preload("../resources/chara1.png", "../resources/map0.png","../resources/chara2.png");
+    game.preload("chara1.png", "map0.png","chara2.png");
     game.onload = function(){
         //設定値
         var END_TIME=400;
-        var GAME_OVER=0; //1:GOAL 2:LOST
+        var GAME_STATUS=0; //1:GOAL 2:LOST
         //マップを設定
         var map = new Map(16, 16);
-        map.image = game.assets["../resources/map0.png"];
+        map.image = game.assets["map0.png"];
         map.loadData(
             [
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -33,7 +33,7 @@ window.onload = function(){
 
         //操作キャラクターを設定
         var bear = new Sprite(32, 32);
-        bear.image = game.assets["../resources/chara1.png"];
+        bear.image = game.assets["chara1.png"];
         bear.x = 0;
         bear.y = 100;
         bear.frame = 0;
@@ -41,7 +41,7 @@ window.onload = function(){
 
         bear.addEventListener("enterframe", function(){
             //操作キャラクターの進むスピードを指定
-            if(GAME_OVER==0){
+            if(GAME_STATUS==0){
                 bear.x+=1;
             }
 
@@ -54,9 +54,9 @@ window.onload = function(){
                 this.y += 32;
             }
             //フレーム
-            if(GAME_OVER==1){
+            if(GAME_STATUS==1){
                 this.frame = 5;
-            } else if(GAME_OVER==2){
+            } else if(GAME_STATUS==2){
                 this.frame = 8;
             } else {
                 this.frame = this.age % 2 + 6;
@@ -65,12 +65,12 @@ window.onload = function(){
 
         //敵キャラクター1を設定
         var pig1 = new Sprite(32, 32);
-        pig1.image = game.assets["../resources/chara2.png"];
+        pig1.image = game.assets["chara2.png"];
         pig1.x = 150;
         pig1.y = 100;
         pig1.frame = 0;
         game.rootScene.addChild(pig1);
-        if(GAME_OVER==0){
+        if(GAME_STATUS==0){
             pig1.addEventListener("enterframe", function(){
                 //フレーム
                 this.frame = this.age % 2 + 6;
@@ -83,7 +83,7 @@ window.onload = function(){
                 //当たり判定
                 for (var i=1;i<2;i++){
                     if(pig1.intersect(bear)){
-                        GAME_OVER=2
+                        GAME_STATUS=2
                     }
                 }
             });
@@ -91,12 +91,12 @@ window.onload = function(){
 
         //敵キャラクター2を設定
         var pig2 = new Sprite(32, 32);
-        pig2.image = game.assets["../resources/chara2.png"];
+        pig2.image = game.assets["chara2.png"];
         pig2.x = 300;
         pig2.y = 80;
         pig2.frame = 0;
         game.rootScene.addChild(pig2);
-        if(GAME_OVER==0){
+        if(GAME_STATUS==0){
             pig2.addEventListener("enterframe", function(){
                 //フレーム
                 this.frame = this.age % 2 + 6;
@@ -109,7 +109,7 @@ window.onload = function(){
                 //当たり判定
                 for (var i=1;i<2;i++){
                     if(pig2.intersect(bear)){
-                        GAME_OVER=2
+                        GAME_STATUS=2
                     }
                 }
             });
@@ -118,9 +118,9 @@ window.onload = function(){
         //MAPの動きを設定
         map.addEventListener("enterframe", function(){
             //ゴール判定
-            if(GAME_OVER==0){
+            if(GAME_STATUS==0){
                 if(this.age==END_TIME){
-                    GAME_OVER=1;
+                    GAME_STATUS=1;
                     bear.x-=1;
                 } else if(bear.x>64){
                     //スクロール
