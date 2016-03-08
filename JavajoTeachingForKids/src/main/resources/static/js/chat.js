f9feaa6c584d5542db4384a2d057194de5d56bff
erk5 <e.hirajiri@gmail.com>
2016-03-08 23:58:23 +0900

    Review. Delete the ver of local variables.

--- JavajoTeachingForKids/src/main/resources/static/js/chat.js
+++ JavajoTeachingForKids/src/main/resources/static/js/chat.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     stompClient.connect({ }, function(frame) {
         // subscribe to the /topic/message endpoint
         stompClient.subscribe("/topic/message", function(data) {
-            var messages = JSON.parse(data.body);
+            messages = JSON.parse(data.body);
             $("<tr>"
                 + "<td>" + messages.date + "</td>"
                 + "<td>" + messages.name + "</td>"
@@ -17,8 +17,8 @@ $(document).ready(function() {
     });
 
     $("button#send").click(function () {
-        var name = $('#name').val();
-        var message = $('#message').val();
+        name = $('#name').val();
+        message = $('#message').val();
         if (name != "" && message != "") {
             stompClient.send('/topic/message', {}, JSON.stringify({"date": new Date().toLocaleString(), "name": name, "message": message}));
             $('#message').val('');
