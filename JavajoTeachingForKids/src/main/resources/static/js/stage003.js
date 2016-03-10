7fb854b8b15bc571b66cec8a6855b95aff0ec821
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-10 12:21:00 +0900

    くまを動かせるように変更

--- JavajoTeachingForKids/src/main/resources/static/js/stage003.js
+++ JavajoTeachingForKids/src/main/resources/static/js/stage003.js
@@ -1,9 +1,11 @@
 enchant();
 
-window.onload = function() {
+var i = 1;
+
+function loadStage(selectVal) {
     var core = new Core(320, 320);
     core.preload('chara1.png');
-    core.fps = 10;
+    core.fps = 8;
 
     var defaultColor = document.getElementsByClassName('color-picker')[0].value;
     core.rootScene.backgroundColor = "#" + defaultColor;
@@ -15,11 +17,15 @@ window.onload = function() {
         bear.y = 0;
 
         bear.on('enterframe', function() {
-//            bearMove.auto(this, core);
-            bearMove.manual(this, core);
+            if (selectVal === 'auto') {
+                bearMove.auto(this, core);
+            } else if (selectVal === 'manual') {
+                bearMove.manual(this, core);
+            }
         });
 
         core.rootScene.addChild(bear);
+
 //        bear.frame = 0;
         bear.frame = [0, 1, 0, 2];
 //        bear.frame = [5, 6, 5, 7];
@@ -27,17 +33,18 @@ window.onload = function() {
     };
 
     core.start();
-};
+    i++;
+}
 
 var bearMove = {
     auto: function(bear, core) {
-        bear.x += 10;
+        bear.x += 10/i;
         if (bear.x > core.width) bear.x = 0;
     },
     manual: function(bear, core) {
-        if (core.input.left) bear.x -= 10;
-        if (core.input.right) bear.x += 10;
-        if (core.input.up) bear.y -= 10;
-        if (core.input.down) bear.y += 10;
+        if (core.input.left) bear.x -= 10/i;
+        if (core.input.right) bear.x += 10/i;
+        if (core.input.up) bear.y -= 10/i;
+        if (core.input.down) bear.y += 10/i;
     }
 };
--- JavajoTeachingForKids/src/main/resources/static/js/stage003_action.js
+++ JavajoTeachingForKids/src/main/resources/static/js/stage003_action.js
@@ -1,6 +1,17 @@
 $(function() {
+    $(window).load(function() {
+        loadStage('notMove');
+    });
+
     $('.color-picker').change(function() {
         var color = $(this).val();
         $('#enchant-stage > div').css('background-color', "#" + color);
+    });
+
+    $('.select-move').change(function() {
+        var selectVal = $('.select-move').val();
+        loadStage(selectVal);
+        $('#enchant-stage').css('height', '0');
+        $('#enchant-stage > div').css('transform', 'scale(1.875)');
     });
 });
