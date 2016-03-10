e3700caaa5d7cddaa43375e60f985ad6d7df6d61
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-10 15:46:12 +0900

    コメント書いた

--- JavajoTeachingForKids/src/main/resources/static/js/stage003.js
+++ JavajoTeachingForKids/src/main/resources/static/js/stage003.js
@@ -1,8 +1,15 @@
 enchant();
 
+// chengeイベント呼び出すごとに値が増えてしまう…
+// （故に微調整でつかう）
 var i = 1;
 
+/**
+ * enchant.jsの描画
+ */
 function loadStage(selectMove, selectBear) {
+
+    // 初期設定
     var core = new Core(320, 320);
     core.preload('chara1.png');
     core.fps = 8;
@@ -10,13 +17,17 @@ function loadStage(selectMove, selectBear) {
     var defaultColor = document.getElementsByClassName('color-picker')[0].value;
     core.rootScene.backgroundColor = "#" + defaultColor;
 
+    // ゲーム本体の描画
     core.onload = function() {
         var bear = new Sprite(32, 32);
         bear.image = core.assets['chara1.png'];
         bear.x = 0;
         bear.y = 0;
 
+        // キャラクターのイベント
         bear.on('enterframe', function() {
+
+            // 画面からのキャラクターの動き変更
             switch(selectMove) {
                 case 'auto':
                     bearMove.auto(this, core);
@@ -31,6 +42,7 @@ function loadStage(selectMove, selectBear) {
 
         core.rootScene.addChild(bear);
 
+        // 画面からのキャラクター変更
         switch(selectBear) {
             case 'notWalk':
                 bear.frame = [0];
@@ -53,11 +65,18 @@ function loadStage(selectMove, selectBear) {
     i++;
 }
 
+/**
+ *  くまの動き
+ */
 var bearMove = {
+
+    // 自動で動く
     auto: function(bear, core) {
         bear.x += 10/i;
         if (bear.x > core.width) bear.x = 0;
     },
+
+    // 十字キーで動く
     manual: function(bear, core) {
         if (core.input.left) bear.x -= 10/i;
         if (core.input.right) bear.x += 10/i;
