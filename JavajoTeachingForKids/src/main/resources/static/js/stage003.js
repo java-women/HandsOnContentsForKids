f1f1d75ae6af0438af5c8a82f37ac298b8519a02
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-12 14:08:18 +0900

    背景色の設定を定数化

--- JavajoTeachingForKids/src/main/resources/static/js/stage003.js
+++ JavajoTeachingForKids/src/main/resources/static/js/stage003.js
@@ -14,7 +14,7 @@ function loadStage(selectMove, selectBear) {
     core.preload('chara1.png');
     core.fps = 8;
 
-    var defaultColor = document.getElementsByClassName('color-picker')[0].value;
+    const defaultColor = document.getElementsByClassName('color-picker')[0].value;
     core.rootScene.backgroundColor = "#" + defaultColor;
 
     // ゲーム本体の描画
